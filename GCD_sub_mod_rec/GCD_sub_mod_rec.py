5db8cb018f7b1d8c2cf50f2717ded77defef630b
Hongsoojeong <my_youth99@naver.com>
2021-01-02 15:49:27 +0900

    GCD의 3가지 알고리즘

---
+++ GCD_sub_mod_rec/GCD_sub_mod_rec.py
@@ -0,0 +1,35 @@
+def gcd_sub(a, b):
+		while(a*b!=0):
+			if(a>b):
+				a=a-b
+			else:
+				b=b-a
+		return a+b
+	
+def gcd_mod(a, b):
+		while(a*b!=0):
+			if(a>b):
+				a=a%b
+			else:
+				b=b%a
+		return a+b
+	
+def gcd_rec(a, b):	
+		if(b==0):
+				return a
+		else:
+				return gcd_rec(b,a%b)
+
+	
+# a, b를 입력받는다
+# gcd_sub, gcd_mod, gcd_rec을 각각 호출하여, x, y, z에 리턴값을 저장한다
+
+a,b=input().split()
+a=int(a)
+b=int(b)
+
+x=gcd_sub(a,b)
+y=gcd_mod(a,b)
+z=gcd_rec(a, b)
+
+print(x, y, z)
